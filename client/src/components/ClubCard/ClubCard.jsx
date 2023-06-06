942ce33e41d839d391ca7d6b79824a1f13637343
aakash414 <67820270+aakash414@users.noreply.github.com>
2023-06-06 20:58:29 +0530

    Club Card styling

--- client/src/components/ClubCard/ClubCard.jsx
+++ client/src/components/ClubCard/ClubCard.jsx
@@ -2,14 +2,14 @@ import "./ClubCard.css"
 
 const ClubCard = () => {
   return (
-    <div className="card-container">
-      <div className="card card--dark">
+    <div className="black-card-container">
+      <div className="black-card card--dark">
         <a href="https://sedscusat.org/">
-          <div className="card--display">
-            <i className="material-icons">SEDS CUSAT</i>
+          <div className="black-card--display">
+            <i className="black-material-icons">SEDS CUSAT</i>
             <h2>The Space Club of Cusat</h2>
           </div>
-          <div className="card--hover">
+          <div className="black-card--hover">
             <h2>SEDS</h2>
             <p>
             SEDS CUSAT is a vibrant community of space enthusiasts who get together to discuss, explore, and have fun with the marvels of the cosmos. SEDS CUSAT organizes and coordinates a spectrum of technical competitions, engineering projects, workshops, social events, seminars, and outreach programs for the SpaceFam.
@@ -17,7 +17,7 @@ const ClubCard = () => {
             <p className="link">Click to know more</p>
           </div>
         </a>
-        <div className="card--border"></div>
+        <div className="black-card--border"></div>
       </div>
     </div>
   );
