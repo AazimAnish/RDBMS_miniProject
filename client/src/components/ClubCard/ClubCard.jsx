5464e34b730f7accb8f900ba3bd45da34adab562
aakash414 <67820270+aakash414@users.noreply.github.com>
2023-06-06 18:36:51 +0530

    Changed ClubCard

--- client/src/components/ClubCard/ClubCard.jsx
+++ client/src/components/ClubCard/ClubCard.jsx
@@ -1,28 +1,23 @@
-import "./ClubCard.css";
+import "./ClubCard.css"
+
 const ClubCard = () => {
   return (
-    <div className="agroupvariant2">
-      <div className="divpx-8">
-        <div className="club-name">
-          <b className="seds-cusat">SEDS CUSAT</b>
-        </div>
-        <div className="p2">
-          <div className="about-club">
-            <p className="get-paid-fast">
-              Get paid fast and keep all the money you earn. Simplify
-            </p>
-            <p className="get-paid-fast">
-              your invoicing process, say goodbye to transaction fees,
-            </p>
-            <p className="get-paid-fast">
-              and make payments a breeze for your customers.
-            </p>
+    <div className="card-container">
+      <div className="card card--dark">
+        <a href="https://sedscusat.org/">
+          <div className="card--display">
+            <i className="material-icons">SEDS CUSAT</i>
+            <h2>The Space Club of Cusat</h2>
           </div>
-        </div>
-      </div>
-      <div className="divoverflow-hidden" />
-      <div className="divinline-flex">
-        <div className="learn-more">Learn more</div>
+          <div className="card--hover">
+            <h2>SEDS</h2>
+            <p>
+            SEDS CUSAT is a vibrant community of space enthusiasts who get together to discuss, explore, and have fun with the marvels of the cosmos. SEDS CUSAT organizes and coordinates a spectrum of technical competitions, engineering projects, workshops, social events, seminars, and outreach programs for the SpaceFam.
+            </p>
+            <p className="link">Click to know more</p>
+          </div>
+        </a>
+        <div className="card--border"></div>
       </div>
     </div>
   );
