ddc84ef0d12e503ba3ab30dc672cf15e6ffdd85f
AazimAnish <aazimanish1@gmail.com>
2023-06-08 00:17:24 +0530

    create circle info

--- client/src/pages/learningCircle/CreateCircle/CreateCircle.jsx
+++ client/src/pages/learningCircle/CreateCircle/CreateCircle.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import './CreateCircle.css';
 import Navbar from "../../../Components/Navbar/Navbar";
 import learncircle from '../../../../public/learningcircles.jpg'
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
 
 
 const CreateCircle = () => {
@@ -32,10 +37,184 @@ const CreateCircle = () => {
             </a>
           </div>
         </div>
+
+
+
+
+            <div className='second_form_section'>
+              <div className='ff_content'>
+                <p className='ff_heading'>Circle Creation Information</p>
+                <p className='ff_tagline'>
+                  Fill in the details and meeting place and
+                  time to continue
+                </p>
+              </div>
+              <div className='ff_form_fields'>
+                <TextField
+                  sx={{
+                    minWidth: 300,
+                    maxWidth: 300,
+                    margin: 1.5,
+                    marginLeft: 0,
+                  }}
+                  required
+                  // error={
+                  //   errors && JSON.stringify(errors).includes("lead.name")
+                  //     ? true
+                  //     : false
+                  // }
+                  // helperText={
+                  //   errors && JSON.stringify(errors).includes("lead.name")
+                  //     ? "Lead Name is Required"
+                  //     : ""
+                  // }
+                  name="name"
+                  id="outlined-basic"
+                  label="Circle Name"
+                  variant="outlined"
+                  // value={create.name}
+                  // onChange={leadchangeHandler}
+                />
+                {/* <TextField
+                  sx={{
+                    minWidth: 300,
+                    maxWidth: 300,
+                    margin: 1.5,
+                    marginLeft: 0,
+                  }}
+                  required
+                  // error={
+                  //   errors && JSON.stringify(errors).includes("lead.email")
+                  //     ? true
+                  //     : false
+                  // }
+                  // helperText={
+                  //   errors && JSON.stringify(errors).includes("lead.email")
+                  //     ? "Lead Email Address is Required"
+                  //     : ""
+                  // }
+                  name="email"
+                  id="outlined-basic"
+                  label="Email Address"
+                  variant="outlined"
+                  // value={create.email}
+                  // onChange={leadchangeHandler}
+                /> */}
+
+                {/* <TextField
+                  required
+                  sx={{
+                    minWidth: 300,
+                    maxWidth: 300,
+                    margin: 1.5,
+                    marginLeft: 0,
+                  }}
+                  name="phone"
+                  id="outlined-basic"
+                  label="Phone"
+                  variant="outlined"
+                  // value={create.phone}
+                  // onChange={changeHandler}
+                /> */}
+                <TextField
+                  required
+                  sx={{
+                    minWidth: 300,
+                    maxWidth: 300,
+                    margin: 1.5,
+                    marginLeft: 0,
+                  }}
+                  name="meet_place"
+                  id="outlined-basic"
+                  label="Meet Place"
+                  placeholder="A Place in your campus to meet"
+                  variant="outlined"
+                  // value={create.meet_place}
+                  // onChange={changeHandler}
+                />
+                <TextField
+                  required
+                  sx={{
+                    minWidth: 300,
+                    maxWidth: 300,
+                    margin: 1.5,
+                    marginLeft: 0,
+                  }}
+                  name="meet_time"
+                  id="outlined-basic"
+                  label="Meet Time"
+                  placeholder="HH:MM AM/PM"
+                  variant="outlined"
+                  // value={create.meet_time}
+                  // onChange={changeHandler}
+                />
+
+                <TextField
+                  sx={{
+                    minWidth: 300,
+                    maxWidth: 300,
+                    margin: 1.5,
+                    marginLeft: 0,
+                  }}
+                  required
+                  name="type"
+                  // error={
+                  //   errors && JSON.stringify(errors).includes("passcode")
+                  //     ? true
+                  //     : false
+                  // }
+                  // helperText={
+                  //   errors && JSON.stringify(errors).includes("passcode")
+                  //     ? "Team Secret Key is Required"
+                  //     : ""
+                  // }
+                  id="outlined-basic"
+                  label="Type of Meet"
+                  placeholder="Online/Offline"
+                  variant="outlined"
+                  // value={create.passcode}
+                  // onChange={changeHandler}
+                />
+
+                {/* <TextField
+                  sx={{
+                    minWidth: 300,
+                    maxWidth: 300,
+                    margin: 1.5,
+                    marginLeft: 0,
+                  }}
+                  required
+                  name="cpasscode"
+                  id="outlined-basic"
+                  label="Confirm Secret Key"
+                  variant="outlined"
+                  // error={
+                  //   confirm !== create.passcode &&
+                  //   confirm &&
+                  //   create.passcode &&
+                  //   confirm.length > 0
+                  //     ? true
+                  //     : false
+                  // }
+                  // helperText={
+                  //   confirm !== create.passcode &&
+                  //   confirm &&
+                  //   create.passcode &&
+                  //   confirm.length > 0
+                  //     ? "Both Screct Keys Should be the Same"
+                  //     : ""
+                  // }
+                  // onChange={(e) => {
+                  //   setConfirm(e.target.value);
+                  // }}
+                /> */}
+              </div>
+            </div>
+
+
         </div>
 
-
-    </div>
+                </div>
   );
 };
 
