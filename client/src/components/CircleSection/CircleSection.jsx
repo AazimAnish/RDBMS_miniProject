d7734a4d884f3ab9b4cba423cb3dba002d6afd13
aakash414 <67820270+aakash414@users.noreply.github.com>
2023-06-09 19:25:29 +0530

    Circle Section Design

--- client/src/components/CircleSection/CircleSection.jsx
+++ client/src/components/CircleSection/CircleSection.jsx
@@ -2,16 +2,53 @@ import CircleCard from "../CircleCard/CircleCard"
 import "./CircleSection.css"
 
 const CircleSection = () => {
+
+  const circles = [
+    {
+      id: "1",
+      head: "Javascript",
+      hidhead: "Javascript",
+      circleName:"Javascript",
+    },{
+      id: "2",
+      head: "React",
+      hidhead: "React",
+      circleName:"React",
+    },{
+      id: "3",
+      head: "DBMS",
+      hidhead: "DBMS",
+      circleName:"DBMS",
+    },{
+      id: "4",
+      head: "PHP",
+      hidhead: "PHP",
+      circleName:"PHP",
+    },{
+      id: "5",
+      head: "Software Design",
+      hidhead: "Software Design",
+      circleName:"Software Design",
+    },{
+      id: "6",
+      head: "Blockchain",
+      hidhead: "Blockchain",
+      circleName:"Blockchain",
+    }]
+
     return(
         <div className="circle-section">
           <b className="join-a-circle">Join a Circle and Learn</b>
           <div className="circle-container">
-            <CircleCard/>
-            <CircleCard/>
-            <CircleCard/>
-            <CircleCard/>
-            <CircleCard/>
-            <CircleCard/>
+          {circles.map((circle) => (
+              <CircleCard
+                key={circle.id}
+                head={circle.head}
+                hidhead={circle.hidhead}
+                circleName={circle.circleName}
+              />
+            ))}
+            
           </div>
         </div>
     )
